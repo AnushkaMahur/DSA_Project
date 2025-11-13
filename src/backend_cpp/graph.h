5a7c32fd0cca4aa79c4b62a48c35750cf7d5933d
Aashma Yadav <aashmarao18@gmail.com>
2025-11-14 05:22:06 +0530

    Update graph.h

--- src/backend_cpp/graph.h
+++ src/backend_cpp/graph.h
@@ -6,13 +6,14 @@
 #include <map>
 using namespace std;
 
+// Graph that stores product recommendations
 class RecommendationGraph {
 private:
     map<string, vector<string>> adjacencyList;
     
 public:
-    void addEdge(const string& product1, const string& product2);
-    vector<string> getRecommendations(const string& productName, int maxResults = 5);
+    void addEdge(const string& product1, const string& product2);    // Connect two products (means they are related)
+    vector<string> getRecommendations(const string& productName, int maxResults = 5); // Get recommended products for a given product
     void loadRecommendations(const string& filename);
 };
 
