a64399698af3d9b30bb14a0cbef8da83ff48023d
Aashma Yadav <aashmarao18@gmail.com>
2025-11-14 05:14:48 +0530

    Update cart.h

--- src/backend_cpp/cart.h
+++ src/backend_cpp/cart.h
@@ -5,24 +5,26 @@
 #include <vector>
 #include <string>
 
+
 struct CartItem {
-    Product* product;
-    int quantity;
+    Product* product;    // Stores the product pointer + how many of that item
+    int quantity;    // Represents one item inside the cart
 
     CartItem(Product* p, int q) : product(p), quantity(q) {}
 };
 
+//all cart operations (main class)
 class ShoppingCart {
 private:
     std::vector<CartItem> items;
 
 public:
     bool addItem(Product* product, int quantity);
-    bool removeItem(const std::string& productName);
-    void showCart();
-    double getTotal();
+    bool removeItem(const std::string& productName);    //add or remove from the cart
+    void showCart();    //view complete cart items
+    double getTotal();    //total cost of cart
     void checkout(ProductManager& pm);
-    void clear();
+    void clear();    //clear cart
     std::vector<CartItem> getItems();
 
     void saveToFile();   // Save cart to file
